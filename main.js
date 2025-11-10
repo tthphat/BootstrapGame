3a13eed6a8e3f9fc6057d902d6e7fe2e5fc398fc
23120319 <truongthanhphat1103@gmail.com>
2025-11-10 21:01:14 +0700

    push project from local to github

---
+++ main.js
@@ -0,0 +1,30 @@
+
+// Bài Tập 1
+const input1 = document.getElementById("inputAnswer-1");
+const btn1 = document.getElementById("button-1");
+const display1 = document.getElementById("display1");
+
+btn1.addEventListener("click", ()=>{
+    display1.innerHTML = input1.value;
+})
+
+
+// Bài Tập 2
+const input2 = document.getElementById("inputAnswer-2");
+const btn2 = document.getElementById("button-2");
+const display2 = document.getElementById("display2");
+
+btn2.addEventListener("click", ()=>{
+    display2.innerHTML = input2.value;
+})
+
+
+// Bài Tập 3
+const input3 = document.getElementById("inputAnswer-3");
+const btn3 = document.getElementById("button-3");
+const display3 = document.getElementById("display3");
+
+btn3.addEventListener("click", ()=>{
+    display3.innerHTML = input3.value;
+})
+
